9eb847a7e933fb88489f2a3c2fc406dd5b08bd4d
Lukas Tatarunas <lukas@LT-LIT-SC-0022.internal.visma.com>
2019-07-10 15:55:24 +0300

    Starting the Dashboard API

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const data = require('./data.json');
+
+const app = express();
+
+app.use('/', express.static('public'));
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/data', (req, res) => {
+    res.header("Content-Type",'application/json',)
+    res.send(data);
+} );
+
+const port = 3000;
+app.listen(port);
+
+console.log('API server started on: ' + port);
