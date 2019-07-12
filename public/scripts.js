dcb372c2d789c5087c2e526a321f3dcd50c220fc
Lukas Tatarunas <lukas@LT-LIT-SC-0022.internal.visma.com>
2019-07-12 15:32:10 +0300

    Fixed GET request

--- public/scripts.js
+++ public/scripts.js
@@ -1,22 +1,25 @@
-  fetch("http://localhost:3000/data")
-    .then(res => res.json())
-    .then(data => {
-      document.querySelector('#first').innerHTML = data[0].id;
-      document.querySelector('#Mark').innerHTML = data[0].firstName;
-      document.querySelector('#Otto').innerHTML = data[0].lastName;
-      document.querySelector('#mdo').innerHTML = data[0].userName;
-      document.querySelector('#second').innerHTML = data[1].id;
-      document.querySelector('#Jacob').innerHTML = data[1].firstName;
-      document.querySelector('#Thornton').innerHTML = data[1].lastName;
-      document.querySelector('#fat').innerHTML = data[1].userName;
-      document.querySelector('#third').innerHTML = data[2].id;
-      document.querySelector('#Larry').innerHTML = data[2].firstName;
-      document.querySelector('#the-Bird').innerHTML = data[2].lastName;
-      document.querySelector('#twitter').innerHTML = data[2].userName;
-    })
-    .catch(err => console.log(err));
+fetch("http://localhost:3000/data")
+  .then(res => res.json())
+  .then(data => {
+    appendData(data);
+    console.log(data);
+  })
+  .catch(err => console.log(err));
 
-    fetch("http://localhost:3000/data")
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+function appendData(data) {
+  data.map(dt => {
+    let element = document.getElementById(`tr${dt.firstName}`);
+    let th = document.createElement("th");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    th.innerHTML = dt.id;
+    td1.innerHTML = dt.firstName;
+    td2.innerHTML = dt.lastName;
+    td3.innerHTML = dt.userName;
+    element.appendChild(th);
+    element.appendChild(td1);
+    element.appendChild(td2);
+    element.appendChild(td3);
+  })
+}
